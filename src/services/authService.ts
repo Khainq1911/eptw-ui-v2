df79f74515a8f680dd1f817dd53a71cdf630d857
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-07-26 23:59:52 +0700

    feat: Register page (#10)
    Co-authored-by: KhaiNguyen <khaitom1911@.dev@gmail.com>

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,12 @@
-import type { LoginFormType } from "../common/type";
-import { axiosInstance } from "../configs/axios";
+import type { LoginFormType, RegisterDataType } from "@/common/type";
+import { axiosInstance } from "@/configs/axios";
 
 export const loginService = async (loginData: LoginFormType) => {
   const response = await axiosInstance.post("/auth/login", loginData);
+  return response.data;
+};
+
+export const registerService = async (registerData: RegisterDataType) => {
+  const response = await axiosInstance.post("/auth/register", registerData);
   return response.data;
 };
