25a01130d42da842590512eee63d0fbcbd744eff
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-10-27 23:04:29 +0700

    temp create template (#25)
    Co-authored-by: khainq20 <khainq20@fpt.com>

--- src/pages/template-page/components/content-modal.tsx
+++ src/pages/template-page/components/content-modal.tsx
@@ -1,40 +1,24 @@
-import { Flex, Segmented } from "antd";
-import { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button} from "antd";
+
 
 export default function ContentModal() {
-  const [mode, setMode] = useState("list");
-
   return (
-    <div className="h-full flex flex-col p-4">
-      {/* Thanh chọn chế độ */}
-      <Flex justify="center">
-        <Segmented
-          options={[
-            { label: "Danh sách thẻ", value: "list" },
-            { label: "Xem trước", value: "preview" },
-          ]}
-          width={200}
-          value={mode}
-          onChange={(val) => setMode(val as string)}
-          size="middle"
-          className="!bg-gray-100 !rounded-lg [&_.ant-segmented-item-selected]:!bg-blue-500 [&_.ant-segmented-item-selected]:!text-white"
-        />
-      </Flex>
+    <main className="flex-1 overflow-y-auto p-8 bg-slate-50 h-full">
+      <div className="max-w-4xl mx-auto">
+        <div className="mb-8">
+          <h1 className="text-4xl font-bold text-slate-900 mb-2">
+            Permit to Work
+          </h1>
+          <p className="text-slate-600">
+            Quản lý và theo dõi các yêu cầu bảo trì an toàn
+          </p>
+        </div>
 
-      {/* Nội dung chính */}
-      <div className="mt-4 flex-1 overflow-y-auto">
-        {mode === "list" ? (
-          <div className="bg-white border rounded p-4 shadow-sm">
-            <h3 className="font-semibold mb-2">Danh sách thẻ</h3>
-            <p>Hiển thị Collapse / danh sách item ở đây...</p>
-          </div>
-        ) : (
-          <div className="bg-gray-50 border rounded p-4 shadow-sm">
-            <h3 className="font-semibold mb-2">Preview</h3>
-            <p>Hiển thị nội dung preview ở đây...</p>
-          </div>
-        )}
+        <Button className="w-full" type="primary" icon={<PlusOutlined />}>
+           Thêm phần mới
+        </Button>
       </div>
-    </div>
+    </main>
   );
 }
